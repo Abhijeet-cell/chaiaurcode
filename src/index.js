37333e9eb78b99e4421234b2c988a1cde7b060de
Abhijeet Kumar <218581425+AbhijeetKumar-pixel@users.noreply.github.com>
2025-09-02 08:28:31 +0530

    fifthcommit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config({ path: '../.env' }); // src folder se ek level upar .env hai
+import { app } from "./app.js";
+
 
 import connectDB from "./db/index.js";
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 5000,()=>{
+        console.log("Sever is running at port number 1000");
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed",err);
+})
